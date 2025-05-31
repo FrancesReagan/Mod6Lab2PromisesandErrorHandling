8a0fa5def601dfcd515a5376c888e549d1e1ac95
Frances Reagan James <adtgwen@gmail.com>
2025-05-31 05:36:51 -0400

    first commit

---
+++ src/apiSimulator.ts
@@ -0,0 +1,84 @@
+//custom error classes//
+export class NetworkError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "Network Error";
+  }
+}
+
+export class DataError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "DataError";
+  }
+}
+
+//type definitions for the data structure//
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+export interface Review {
+  id: number:
+  productId: number;
+  rating: number;
+  comment: string;
+  reviewer: string;
+}
+
+export interface SalesReport {
+  totalSales: number;
+  unitsSold: number;
+  averagePrice: number;
+}
+
+//simulate fetching product catalog//
+export const fetchProductCatalog = (): Promise<Product[]> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      //use Math.random() to simulate success/failure//
+      if(Math.random() < 0.8) { //80% success rate//
+        resolve([
+          { id: 1, name: "Laptop", price: 1200 },
+          { id: 2, name: "Headphones", price: 198 },
+          {id: 3, name: "Mouse", price: 50 },
+          { id: 4, name: "Keyboard", price: 26 },
+          { id: 5, name: "Monitor", price: 299 },
+          ]);
+      } else {
+        reject(new NetworkError("Failed to fetch product catalog"));
+      }
+    }, 1000); // 1 second delay//
+    });
+  };
+
+  //Simulate fetching product reviews//
+  export const fetchProductReviews = (productId: number): Promise<Review[]> => {
+    return new Promise((resolve, reject)) => {
+      setTimeout(() => {
+        if (Math.random() < 0.7) { //70% success rate//
+          //faux reviews based on productId//
+          const reviews: Review[] = [
+            {
+              id: 1,
+              productId: productId,
+              rating: 5,
+              comment: "Excellent product!",
+              reviewer: "Jane Oaken"
+
+            },
+            {
+              id: 2,
+              productId: productId,
+              rating: 3.8,
+              
+            }
+            
+          ];
+
+        }
+      })
+    }
+  }
